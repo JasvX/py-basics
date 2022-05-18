c0951aa03643a9d9aece2568980e4f352879f584
root <root@localhost>
2022-05-18 17:54:49 -0500

    auto publish 1652914488765

---
+++ src/math.py
@@ -0,0 +1,35 @@
+
+# Operadores matematicos +, -, *, /, % ..
+
+a = 36
+b = 15
+
+print("""OPERADORES +, -, *, /, %..
+a = 36
+b = 15
+
+s1 = "im learning to "
+s2 = "code in python"
+
+""")
+
+# suma
+print('suma a + b: ', a + b)
+
+# resta
+print('resta a - b: ', a - b)
+
+# multiplicacion
+print('multiplicacion a * b: ', a * b)
+
+# division
+print('division a / b: ', a / b)
+
+# modulo
+print('modulo a % b: ', a % b)
+
+# concatenacion(suma de 2 strings)
+s1 = "im learning to "
+s2 = "code in python"
+
+print(s1 + s2)
