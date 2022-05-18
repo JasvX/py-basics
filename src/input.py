0199e415190489e8f2c07026865e6d9ac13563dd
root <root@localhost>
2022-05-18 17:53:46 -0500

    auto publish 1652914426530

---
+++ src/input.py
@@ -0,0 +1,25 @@
+
+#para recibir entrada de datos se utiliza input()
+"""
+  se puede guardar el input en una variable y 
+  mostrarlo despues
+"""
+
+
+#se puede mostrar un mensaje como parametro de input()
+inp = input("escribe algo: ")
+
+print(inp)
+
+#si se espera un numero debe ser primero convertido
+
+n = input("ingresa un numero entero.. ")
+int(n)
+print(n)
+
+#para numeros xon fracciones se utiliza float()
+x = input("ingresa un float: ")
+float(x)
+
+print(x)
+
