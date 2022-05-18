0ba5f68a0ad98d568d3e5ebefcddf5dbf813ec03
root <root@localhost>
2022-05-18 17:51:59 -0500

    auto publish 1652914318992

---
+++ src/variables.py
@@ -0,0 +1,29 @@
+
+
+#uso de variables en python
+
+a = 17
+b = 33
+a = b
+print(a)
+
+#tipos de variables 
+#strings
+s1 = "comillas dobles"
+s2 = 'comillas simples'
+print(s1)
+print(s2)
+
+#numeric
+x = 23
+y = 25.78
+
+print(x)
+print(y)
+
+#boolean
+t = True
+f = False
+
+print(t)
+print(f)
