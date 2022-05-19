05545be57d075ea4371054bb733701bc7fe70d7f
root <root@localhost>
2022-05-18 22:15:12 -0500

    auto publish 1652930111582

---
+++ src/05_conditionals.py
@@ -0,0 +1,33 @@
+a = input()
+a = int(a)
+
+b = input()
+b = int(b)
+
+#para usar condicionales se utilizan if-else
+if a > b: # el valor de if de ser un valor boleano
+    print(a)
+else:
+    print(b)
+    
+    
+# para comparar dos valores se utilizan los sig simbolos
+# > == < !=
+
+if a == b:
+  print("iguales")
+  
+if a != b:
+  print("no son iguales")
+  
+if a > b:
+  print("a es mayor")
+  
+if a < b:
+  print("a es menor")
+
+
+
+
+    
+
