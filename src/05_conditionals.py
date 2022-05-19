c2bd6a0fa9b926f6d9fdf0b696ad71ce163313d5
root <root@localhost>
2022-05-19 10:17:22 -0500

    auto publish 1652973441453

--- src/05_conditionals.py
+++ src/05_conditionals.py
@@ -25,7 +25,17 @@ if a > b:
   
 if a < b:
   print("a es menor")
+  
+  
+# keyword 'and'
 
+salary = 5700
+vacation_days= 23
+
+if salary > 5000 and vacation_days > 40:
+    print ("I will take this job.")
+else: 
+    print("This job sucks! Give it to Jerry.")
 
 
 
